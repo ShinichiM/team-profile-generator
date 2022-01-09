3098494fa87f04ac9a21e10145bd12190b7709e7
Shinichi <miyakawashi@gmail.com>
2022-01-09 16:10:32 -0500

    Test cases and Class implementations for Employee, Engineer, Intern, and Manager

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,26 +1,42 @@
 const Intern = require('../lib/Intern');
 
-describe('Test Parent Describe', () => {
-    describe('Test Describe Child 1', () => {
-        it('Test it Child 1', () => {
-            // TODO: Add Testing 
-            expect()
+describe('Test suite for Intern Class implementation', () => {
+    describe('Ensure Intern contructor successfully processes new object instantances', () => {
+        it('Tests the input and output of the Intern school property', () => {
+            const intern = new Intern('Tomato Jones', 12, 'tj@test.com', 'Ohio State');
+
+
+            expect(intern.name).toStrictEqual(expect.any(String));
+            expect(intern.name).toBe('Tomato Jones');
+            
+            expect(intern.id).toStrictEqual(expect.any(Number));
+            expect(intern.id).toBe(12);
+
+            expect(intern.email).toStrictEqual(expect.any(String));
+            expect(intern.email).toBe('tj@test.com');
+
+            expect(intern.school).toStrictEqual(expect.any(String));
+            expect(intern.school).toBe('Ohio State');
         })
     })
 
 
-    describe('Test Describe Child 2', () => {
-        it('Test it Child 2', () => {
-            // TODO: Add Testing 
-            expect()
+    describe('Ensure getSchool() method of the Intern class returns successfully', () => {
+        it('Tests valid inputs and outputs of the getSchool() method', () => {
+            const intern = new Intern('Tomato Jones', 12, 'tj@test.com', 'Ohio State');
+
+            expect(intern.getSchool()).toStrictEqual(expect.any(String));
+            expect(intern.getSchool()).toBe('Ohio State');
         })
     })
 
 
-    describe('Test Describe Child 3', () => {
-        it('Test it Child 3', () => {
-            // TODO: Add Testing 
-            expect()
+    describe('Ensure getRole() method of the Intern class returns successfully', () => {
+        it('Tests valid inputs and outputs of the getRole() method', () => {
+            const intern = new Intern('Tomato Jones', 12, 'tj@test.com', 'Ohio State'); 
+
+            expect(intern.getRole()).toStrictEqual(expect.any(String));
+            expect(intern.getRole()).toBe('Intern');
         })
     })
 }) 
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,26 +1,32 @@
 const Manager = require('../lib/Manager');
 
-describe('Test Parent Describe', () => {
-    describe('Test Describe Child 1', () => {
-        it('Test it Child 1', () => {
-            // TODO: Add Testing 
-            expect()
+
+describe('Test suite for manager Class implementation', () => {
+    describe('Ensure manager contructor successfully processes new object instantances', () => {
+        it('Tests the input and output of the manager officeNumber property', () => {
+            const manager = new Manager('Tomato Jones', 12, 'tj@test.com', 1092);
+
+
+            expect(manager.name).toStrictEqual(expect.any(String));
+            expect(manager.name).toBe('Tomato Jones');
+            
+            expect(manager.id).toStrictEqual(expect.any(Number));
+            expect(manager.id).toBe(12);
+
+            expect(manager.email).toStrictEqual(expect.any(String));
+            expect(manager.email).toBe('tj@test.com');
+
+            expect(manager.officeNumber).toStrictEqual(expect.any(Number));
+            expect(manager.officeNumber).toBe(1092);
         })
     })
 
+    describe('Ensure getRole() method of the manager class returns successfully', () => {
+        it('Tests valid inputs and outputs of the getRole() method', () => {
+            const manager = new Manager('Tomato Jones', 12, 'tj@test.com', 1092); 
 
-    describe('Test Describe Child 2', () => {
-        it('Test it Child 2', () => {
-            // TODO: Add Testing 
-            expect()
-        })
-    })
-
-
-    describe('Test Describe Child 3', () => {
-        it('Test it Child 3', () => {
-            // TODO: Add Testing 
-            expect()
+            expect(manager.getRole()).toStrictEqual(expect.any(String));
+            expect(manager.getRole()).toBe('Manager');
         })
     })
 }) 
