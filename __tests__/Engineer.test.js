7bb262bc8b098c26f3194e5a1add1942850e64d7
Shinichi <miyakawashi@gmail.com>
2022-01-09 15:04:07 -0500

    Employee class properties and methods as well as correspoding test suite

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,26 @@
+const Engineer = require('../lib/Engineer');
+
+describe('Test Parent Describe', () => {
+    describe('Test Describe Child 1', () => {
+        it('Test it Child 1', () => {
+            // TODO: Add Testing 
+            expect()
+        })
+    })
+
+
+    describe('Test Describe Child 2', () => {
+        it('Test it Child 2', () => {
+            // TODO: Add Testing 
+            expect()
+        })
+    })
+
+
+    describe('Test Describe Child 3', () => {
+        it('Test it Child 3', () => {
+            // TODO: Add Testing 
+            expect()
+        })
+    })
+}) 
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,26 @@
+const Manager = require('../lib/Manager');
+
+describe('Test Parent Describe', () => {
+    describe('Test Describe Child 1', () => {
+        it('Test it Child 1', () => {
+            // TODO: Add Testing 
+            expect()
+        })
+    })
+
+
+    describe('Test Describe Child 2', () => {
+        it('Test it Child 2', () => {
+            // TODO: Add Testing 
+            expect()
+        })
+    })
+
+
+    describe('Test Describe Child 3', () => {
+        it('Test it Child 3', () => {
+            // TODO: Add Testing 
+            expect()
+        })
+    })
+}) 
